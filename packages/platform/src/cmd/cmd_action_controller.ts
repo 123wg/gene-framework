06e98c8acc744008cae413ad9d06b6c878e03167
Gene <38320664+123wg@users.noreply.github.com>
2024-10-27 21:32:01 +0800

    Add CmdMgr

--- packages/platform/src/cmd/cmd_action_controller.ts
+++ packages/platform/src/cmd/cmd_action_controller.ts
@@ -3,6 +3,37 @@ import { DefaultController } from "../controller/default_controller";
 /**
  * Cmd和Action共同的Controller基类
  */
-export class CmdActionController extends DefaultController {
+export class CmdActionController<T> extends DefaultController {
+    /**
+     * 启动的action
+     */
+    public action?:CmdActionController;
 
+    /**
+     * promise结束,cmd才结束
+     */
+    private _status:{
+        promise:Promise<T>,
+        resolve:(result:T)=>void,
+        finish?:boolean
+    };
+
+    /**
+     * 命令执行
+     */
+    public async execute(..._cmdParams:unknown[]){
+
+    }
+
+
+    /**
+     * 命令取消
+     */
+    public cancel(){
+        this._resolve();
+    }
+
+    protected _resolve(){
+
+    }
 }
--- packages/platform/src/cmd/cmd_mgr.ts
+++ packages/platform/src/cmd/cmd_mgr.ts
@@ -1,15 +1,40 @@
 import { I_ProcessEvent } from "../controller/i_process_event";
 import { I_MouseEvent, I_KeyboardEvent } from "../type_define/type_define";
+import { Cmd } from "./cmd";
 
 /**
  * 接收事件
  */
 export class CmdMgr implements I_ProcessEvent{
-    // TODO 提供实现
-    processMouseEvent(_event: I_MouseEvent): boolean {
-        throw new Error("Method not implemented.");
+    private static _instance:CmdMgr;
+
+    private _currentCmd?:Cmd;
+    public static instance():CmdMgr {
+        if(!this._instance){
+            this._instance = new CmdMgr();
+        }
+        return this._instance;
     }
-    processKeyboardEvent(_docevent: I_KeyboardEvent): boolean {
-        throw new Error("Method not implemented.");
+
+    public getCurrentCmd(){
+        return this._currentCmd;
+    }
+
+    public getCurrentController(){
+        let controller = this.getCurrentCmd();
+        while(controller?.action) {
+            controller = controller.action;
+        }
+        return controller;
+    }
+
+
+    public processMouseEvent(event: I_MouseEvent): boolean {
+        const target = this.getCurrentController();
+        return !!target?.processMouseEvent(event);
+    }
+    public processKeyboardEvent(event: I_KeyboardEvent): boolean {
+        const target = this.getCurrentController();
+        return !!target?.processKeyboardEvent(event);
     }
 }
