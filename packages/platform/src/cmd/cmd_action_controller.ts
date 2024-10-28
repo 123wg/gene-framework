b385218fd6c7aff9b38271817a5166f280ccc7db
Gene <2367348689@qq.com>
2024-10-28 10:00:48 +0800

    Complete CmdActionController

--- packages/platform/src/cmd/cmd_action_controller.ts
+++ packages/platform/src/cmd/cmd_action_controller.ts
@@ -2,38 +2,60 @@ import { DefaultController } from "../controller/default_controller";
 
 /**
  * Cmd和Action共同的Controller基类
+ * @template T 返回结果类型
  */
 export class CmdActionController<T> extends DefaultController {
     /**
      * 启动的action
      */
-    public action?:CmdActionController;
+    public action?: CmdActionController<T>;
 
     /**
      * promise结束,cmd才结束
      */
-    private _status:{
-        promise:Promise<T>,
-        resolve:(result:T)=>void,
-        finish?:boolean
+    private _status: {
+        promise: Promise<T | undefined>,
+        resolve: (result: T | undefined) => void,
+        finish?: boolean
     };
+
+    /**
+     * 状态初始化,仅由CmdMgr调用
+     */
+    public initStatus() {
+        this._status.promise = new Promise<T | undefined>(res => this._status.resolve = res);
+        delete this._status.finish;
+
+        return this._status;
+    }
 
     /**
      * 命令执行
      */
-    public async execute(..._cmdParams:unknown[]){
+    public async execute(..._cmdParams: unknown[]) {
 
     }
-
 
     /**
      * 命令取消
      */
-    public cancel(){
+    public cancel() {
         this._resolve();
     }
 
-    protected _resolve(){
+    /**
+     * 命令销毁回调
+     */
+    public onDestroy() { }
 
+    /**
+     * 标记执行结束并返回结果
+     */
+    protected _resolve(data?: T): void {
+        if (this._status.finish) return;
+
+        this._status.finish = true;
+        this._status.resolve(data);
+        this.onDestroy();
     }
 }
