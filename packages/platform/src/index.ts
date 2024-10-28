0101884db8a5f96c10fd89d9a6651c05538fecf4
Gene <2367348689@qq.com>
2024-10-28 13:19:31 +0800

    Add platform export

---
+++ packages/platform/src/index.ts
@@ -0,0 +1,8 @@
+// cmd
+export { ActionResult } from "./cmd/action_result";
+export { Action } from "./cmd/action";
+export { CmdMgr } from "./cmd/cmd_mgr";
+export { Cmd } from "./cmd/cmd";
+
+//controller
+
