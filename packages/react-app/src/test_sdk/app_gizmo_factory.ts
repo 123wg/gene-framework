4bd05bb1d92393b7ca2650ec3909e09e2512cd24
Gene <38320664+123wg@users.noreply.github.com>
2024-11-24 14:57:03 +0800

    feat: fix app_gizmo_factory

--- packages/react-app/src/test_sdk/app_gizmo_factory.ts
+++ packages/react-app/src/test_sdk/app_gizmo_factory.ts
@@ -1,4 +1,4 @@
-import { Element, GRect, GRep, TmpElementPainter } from "@gene/core";
+import { CoreConfig, Element, GRect, GRep, TmpElementPainter } from "@gene/core";
 import { app, GizmoFactory } from "@gene/platform";
 import { GizmoBase } from "@gene/render";
 import { ImageElement } from "./image/image_element";
@@ -10,17 +10,15 @@ export class AppGizmoFactory extends GizmoFactory {
         if (elements.length === 1) {
             const element = elements[0];
             if (element.isLike(ImageElement) || element.isLike(CircleElement)) {
-                // console.log(element.getGRep().getClientRect());
-                const tmp = new TmpElementPainter(app.doc);
-                const grep = new GRep();
-                const gRect = new GRect(element.getGRep().getClientRect());
-                gRect.setStyle({ stroke: 'red', strokeWidth: 10 });
+                // const tmp = new TmpElementPainter(app.doc);
+                // const grep = new GRep();
+                // const rect = element.getGRep().getClientRect();
+                // const gRect = new GRect(rect);
+                // gRect.setStyle({ stroke: CoreConfig.previewPointStroke, strokeWidth: 1 });
 
-                grep.addNode(gRect);
-                tmp.drawTmpGRep(grep);
-                app.doc.updateView();
-
-                // gizmos.push();
+                // grep.addNode(gRect);
+                // tmp.drawTmpGRep(grep);
+                // app.doc.updateView();
             }
         }
 
