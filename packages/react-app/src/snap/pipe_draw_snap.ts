ef82a3a74288e989c4d3f98211a73d1c0aedcc9f
Gene <2367348689@qq.com>
2024-12-05 10:49:05 +0800

    feat: snap_geo_helper add some common methods

--- packages/react-app/src/snap/pipe_draw_snap.ts
+++ packages/react-app/src/snap/pipe_draw_snap.ts
@@ -1,12 +1,22 @@
+import { Vec2 } from "@gene/core";
 import { SnapBase } from "@gene/core/src/snap/snap_base";
 import { T_SnapResult } from "@gene/core/src/type_define/type_define";
 
 /**
- * 管道吸附
- * 要求能实现吸附所有物体包围盒点、特征点、画线时上一个点构成的角度线
+ * 管道绘制吸附
+ * 吸所有点和上一个点固定角度组成的线
  */
 export class PipeDrawSnap extends SnapBase {
+    private _mPoint: Vec2;
+
+    private _previous: Vec2 | undefined;
+    constructor(mPoint: Vec2, previous?: Vec2) {
+        super();
+        this._mPoint = mPoint;
+        this._previous = previous;
+    }
+
     public doSnap(): T_SnapResult {
-        throw new Error("Method not implemented.");
+
     }
 }
---
+++ packages/react-app/src/snap/snap_geo_helper.ts
@@ -0,0 +1,62 @@
+import { Ln2, MathUtil, TransformElement, Vec2 } from "@gene/core";
+import { PipeElement } from "@gene/editor-sdk";
+import { app } from "@gene/platform";
+
+/**
+ * 获取吸附时几何数据的辅助类
+ */
+export class SnapGeoHelper {
+    /**
+     * 根据点生成45度间隔线
+     */
+    public static getAngleLineByPoint(point: Vec2) {
+        const lines: Ln2[] = [];
+        for (let i = 0; i < 4; i += 1) {
+            const dir = Vec2.X().vecRotate(Math.PI / 4 * i);
+            const line = new Ln2(point, point.added(dir));
+            lines.push(line);
+        }
+        return lines;
+    }
+
+    /**
+     * 获取所有管道点
+     */
+    public static getAllPipePoints() {
+        const pipes = app.doc.getAllElementsByCtor(PipeElement);
+        const pPoints = pipes.reduce<Vec2[]>((points, pipe) => {
+            const arr = pipe.points.map(p => new Vec2(p));
+            return points.concat(arr);
+        }, []);
+        return pPoints;
+    }
+
+    /**
+     * 获取变换图元变换后角点
+     */
+    public static getAllTransformedCorners() {
+        const transEles = app.doc.getAllElementsByCtor(TransformElement);
+        const points: Vec2[] = [];
+        transEles.forEach(ele => {
+            const transform = ele.getTransform();
+            const rect = ele.getGRep().getClientRect();
+            const rPoints = MathUtil.getCornerPoints(rect);
+            rPoints.forEach(p => {
+                const realPos = transform.point(p);
+                points.push(new Vec2(realPos));
+            });
+        });
+        return points;
+    }
+
+    /**
+     * 获取绘制管道时可吸附点
+     */
+    public static getPipeDrawSnapPoints() {
+        const points: Vec2[] = [];
+        const ps1 = this.getAllPipePoints();
+        const ps2 = this.getAllTransformedCorners();
+        points.concat(ps1, ps2);
+        return points;
+    }
+}
